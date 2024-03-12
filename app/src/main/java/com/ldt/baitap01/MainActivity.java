894ac9936fe671aa9c6be17f95225170cfd20277
tuoitho <tuoithokhoc1414@gmail.com>
2024-03-12 23:14:15 +0700

    update

---
+++ app/src/main/java/com/ldt/baitap01/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ldt.baitap01;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ImageView imageView = findViewById(R.id.profile_image);
+        TextView textView = findViewById(R.id.textView);
+        imageView.setImageResource(R.drawable.ic_launcher_foreground);
+        textView.setText("Lê Đình Trí 22110442");
+        imageView.setImageResource(R.drawable.avatar_3);
+    }
+}
